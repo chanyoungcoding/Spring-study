d764f50fa331d9a5537b1a63d135c2d01a08305e
chan <white1614@inzent.com>
2025-01-06 16:28:40 +0900

    자동 의존성 주입 및 @Component Test

--- src/test/java/hello/core/discount/RateDiscountPolicyTest.java
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -1,14 +1,19 @@
 package hello.core.discount;
 
+import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 class RateDiscountPolicyTest {
 
-    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+    ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+    DiscountPolicy discountPolicy = applicationContext.getBean("discountPolicy", DiscountPolicy.class);
+//    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     @Test
     @DisplayName("VIP 는 10% 할인이 적용되어야 합니다.")
@@ -35,6 +40,15 @@ class RateDiscountPolicyTest {
         //then
         Assertions.assertThat(discount).isEqualTo(0);
 
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너의 싱글톤 확인")
+    void springContainer() {
+        DiscountPolicy discountPolicy1 = applicationContext.getBean("discountPolicy", DiscountPolicy.class);
+        DiscountPolicy discountPolicy2 = applicationContext.getBean("discountPolicy", DiscountPolicy.class);
+
+        Assertions.assertThat(discountPolicy1).isSameAs(discountPolicy2);
     }
 
 }
