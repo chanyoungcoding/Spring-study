9a5183e65810b29a6edb3a687cc60662ca495a58
chan <white1614@inzent.com>
2025-01-16 15:36:27 +0900

    @Controller and @RestController diffent Test

--- src/main/java/hello/core/PlayerApp.java
+++ src/main/java/hello/core/PlayerApp.java
@@ -2,21 +2,63 @@ package hello.core;
 
 import hello.core.common.FootballTeam;
 import hello.core.common.Player;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController
+//@RestController
+//public class PlayerApp {
+//
+//  @GetMapping("/player")
+//  public Player getOnePlayer() {
+//    return new Player("chan", 28);
+//  }
+//
+//  @GetMapping("/players")
+//  public List<Player> getAllPlayer() {
+//    Player player1 = new Player("chan", 28);
+//    Player player2 = new Player("kim", 27);
+//    Player player3 = new Player("young", 26);
+//
+//    ArrayList<Player> list = new ArrayList<>();
+//    list.add(player1);
+//    list.add(player2);
+//    list.add(player3);
+//
+//    return list;
+//  }
+//
+//  @GetMapping("/team")
+//  public FootballTeam getOneTeam() {
+//    Player player1 = new Player("Son Heungmin", 7 );
+//    Player player2 = new Player("Harry Kane", 10);
+//    Player player3 = new Player("Hugo Lloris", 1);
+//
+//    ArrayList<Player> players = new ArrayList<>();
+//    players.add(player1);
+//    players.add(player2);
+//    players.add(player3);
+//
+//    return new FootballTeam("Tottenham", "Premier League", 5, players);
+//  }
+//
+//}
+
+@Controller
 public class PlayerApp {
 
   @GetMapping("/player")
+  @ResponseBody
   public Player getOnePlayer() {
     return new Player("chan", 28);
   }
 
   @GetMapping("/players")
+  @ResponseBody
   public List<Player> getAllPlayer() {
     Player player1 = new Player("chan", 28);
     Player player2 = new Player("kim", 27);
@@ -31,6 +73,7 @@ public class PlayerApp {
   }
 
   @GetMapping("/team")
+  @ResponseBody
   public FootballTeam getOneTeam() {
     Player player1 = new Player("Son Heungmin", 7 );
     Player player2 = new Player("Harry Kane", 10);
--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -3,18 +3,16 @@ package hello.core.web;
 import hello.core.common.MyLogger;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class LogDemoController {
 
   private final MyLogger myLogger;
 
   @RequestMapping("log-demo")
-  @ResponseBody
   public String logDemo(HttpServletRequest request) {
     String requestUrl = request.getRequestURL().toString();
     myLogger.setRequestURL(requestUrl);
