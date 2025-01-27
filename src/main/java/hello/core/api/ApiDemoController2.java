94cfafad0cdb378733186661b48e1e2bc17b0afc
chanyoungcoding <white1614@naver.com>
2025-01-27 13:19:22 +0900

    RestController Test

---
+++ src/main/java/hello/core/api/ApiDemoController2.java
@@ -0,0 +1,45 @@
+package hello.core.api;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+public class ApiDemoController2 {
+
+
+    private final MemberRepository memberRepository;
+    private final MemberService memberService;
+    private final OrderService orderService;
+
+    @GetMapping("/apiTest")
+    @ResponseBody
+    public Member apiDemo() {
+
+        Member member = new Member(1L, "chan", Grade.VIP);
+        memberRepository.save(member);
+
+        return memberService.findMember(1L);
+    }
+
+    @GetMapping("/apiTest2")
+    @ResponseBody
+    public Order apiDemo2() {
+
+        long memberId = 1;
+        Member member = new Member(1L, "chan", Grade.VIP);
+
+        memberService.join(member);
+
+        return orderService.createOrder(memberId, "itemA", 10000);
+
+    }
+}
